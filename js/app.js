9f2664cf875d4a7073e69a6c526922f35cb5425c
Erika Joy De La Cruz <erikajoy.odc@gmail.com>
2021-03-22 16:36:43 -0700

    created functionality
    created functions and fetched the data from the API

--- js/app.js
+++ js/app.js
@@ -5,18 +5,67 @@ const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");
 
-/*
-fetch(urlAPI)
-    .then(res => res.json())
-    .then(res => res.results)
-    .then(displayEmployees)
-    .catch(err => console.log(err))
-    */
+fetch(urlAPI) //fetches data from urlAPI website
+    .then(res => res.json()) //formats response as JSON
+    .then(res => res.results) //returns results of the response
+    .then(displayEmployees) //passes results into displayEmployees function to create the HTML markup
+    .catch(err => console.log(err)); //console logs errors
 
-$.ajax({
-    url: 'https://randomuser.me/api',
-    dataType: 'json',
-    success: function(data) {
-        console.log(data);
+function displayEmployees(employeeData) {
+    employees = employeeData;
+    let employeeHTML = ''; //stores employee HTML
+    employees.forEach((employee, index) => { //loops through each employee and creates HTML
+        let name = employee.name;
+        let email = employee.email;
+        let city = employee.location.city;
+        let picture = employee.picture;
+        
+        //HTML markup that's being added
+        employeeHTML += ` 
+            <div class="card" data-index = "${index}">
+                <img class="avatar" src="${picture.large}"/>
+                <div class="text-container">
+                    <h2 class="name">${name.first} ${name.last}</h2>
+                    <p class="email">${email}</p>
+                    <p class="address">${city}</p>
+                </div>
+            </div> 
+        `
+    });
+
+    gridContainer.innerHTML = employeeHTML;
+};
+
+function displayModal(index) {
+
+    let {name, dob, phone, email, location: {city, street, state, postcode}, picture } = employees[index];
+    let date = new Date(dob.date);
+    const modalHTML = `
+        <img class="avatar" src="${picture.large}">
+        <div class="text-container">
+            <h2 class="name">${name.first} ${name.last}</h2>
+            <p class="email">${email}</p>
+            <p class="address">${city}</p>
+            <hr/>
+            <p>${phone}</p>
+            <p class="address">${street.name}, ${street.number}, ${city} ${state} ${postcode}</p>
+            <p> Birthday:
+                ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+            </div>
+    ` ;
+    overlay.classList.remove("hidden");
+    modalContainer.innerHTML = modalHTML;
+};
+
+gridContainer.addEventListener('click', e => {
+    if (e.target !== gridContainer) {
+        const card = e.target.closest(".card");
+        const index = card.getAttribute("data-index");
+
+        displayModal(index);
     }
-})
+});
+
+modalClose.addEventListener("click", () => {
+    overlay.classList.add("hidden");
+});
