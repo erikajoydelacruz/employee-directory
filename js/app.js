6bf87d08af91dbe17a34288fcce0924209e94bb4
Erika Joy De La Cruz <erikajoy.odc@gmail.com>
2021-03-21 18:36:09 -0700

    Update app.js

---
+++ js/app.js
@@ -0,0 +1,22 @@
+let employees = [];
+const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US`;
+const gridContainer = document.querySelector(".grid-container");
+const overlay = document.querySelector(".overlay");
+const modalContainer = document.querySelector(".modal-content");
+const modalClose = document.querySelector(".modal-close");
+
+/*
+fetch(urlAPI)
+    .then(res => res.json())
+    .then(res => res.results)
+    .then(displayEmployees)
+    .catch(err => console.log(err))
+    */
+
+$.ajax({
+    url: 'https://randomuser.me/api',
+    dataType: 'json',
+    success: function(data) {
+        console.log(data);
+    }
+})
